45e88197be73c577d1ad88a2b6375b4a9d97f4ee
Carissa Sarreal <carissa.sarreal@gmail.com>
2020-09-27 08:49:40 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,13 +2,13 @@ require 'pry'
 
 class Song
   attr_accessor :name
-  attr_reader :artist
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
+  extend Findable
+  include Paramable
+  
 
   @@songs = []
-
-  def initialize
-    @@songs << self
-  end
 
 
   def self.all
